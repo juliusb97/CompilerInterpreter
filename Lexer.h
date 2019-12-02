9c8fe01053ee306ce596ce87127f883addfa6a90
Julius B <juliusthefirst@gmail.com>
2019-12-02 18:08:20 +0100

    Revert "CL-2019.2.5 <julius@julius-VirtualBox Overwrite remote https://github.com/silver1bear/CompilerInterpreter.git to local"
    This reverts commit 17ced678a148c5c97657421a56686e039d4a742f.

---
+++ Lexer.h
@@ -0,0 +1,131 @@
+//
+// Created by julius on 11/14/19.
+//
+
+#ifndef HELLOWORLD_LEXER_H
+#define HELLOWORLD_LEXER_H
+
+#endif //HELLOWORLD_LEXER_H
+
+
+#define FAIL -1
+#define OK 0
+#define MAXKEYWORDCLASSLENGTH 3
+#define NOKEYWORD -1
+
+static char* keyWords[] =
+{
+	"BEGIN",    "CALL",     "CONST",    "DO",   "END",  "IF",
+	"ODD",      "PROCEDURE","THEN",     "VAR",  "WHILE"
+};
+
+/*
+Character classes:
+ 0 - continue
+ 1 - special character (in Z0) except '='
+ 2 - Alpha
+ 3 - Numeral
+ 4 - ':'
+ 5 - '<'
+ 6 - '>'
+ 7 - '='
+*/
+
+static char cLookupTable[] =
+{
+/*         0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F*/
+/*0 */     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+/*10*/     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+/*20*/     0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+/*30*/     3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 1, 5, 7, 6, 1,
+/*40*/     0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+/*50*/     2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0,
+/*60*/     0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+/*70*/     2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0
+};
+
+static int keywordClasses[][MAXKEYWORDCLASSLENGTH] =
+{
+/*0*/	{-1, -1, -1},
+/*1*/   {-1, -1, -1},
+/*2*/   { 5,  3, -1},
+/*3*/   { 4,  6,  9},
+/*4*/   { 1,  8, -1},
+/*5*/   { 0,  2, 10},
+/*6*/   {-1, -1, -1},
+/*7*/   {-1, -1, -1},
+/*8*/   {-1, -1, -1},
+/*9*/   { 7, -1, -1}
+};
+
+//typedef enum T_Fx {ifl=0x0, ifb=0x10, ifgl=0x20, ifsl=0x30, ifslb=0x40} tFx;
+typedef enum T_MC{mcEmpty, mcSymb, mcNum, mcIdent, mcEOF} tMC;
+typedef enum T_ZS{
+	zNIL,
+	zERG = 128, zLE, zGE,
+	zBGN, zCLL, zCST, zDO, zEND, zIF, zODD, zPRC, zTHN, zVAR, zWHL
+}tZS;
+
+typedef struct{
+	tMC MC;                     //Morphemcode
+	int PosLine;                //Line
+	int PosCol;                 //Column
+	union VAL{
+		long Num;
+		char* pStr;
+		unsigned long Symb;
+	}Val;
+	int MLen;
+} tMorph;
+
+static void        finalize (void);
+/*
+static void        fl       (void);
+static void        fb       (void);
+static void        fgl      (void);
+static void        fsl      (void);
+static void        fslb     (void);
+*/
+void               readc    (void);
+void               writec   (void);
+int                initLex  (char* fname);
+tMorph*            Lex      (void);
+
+typedef void (*FX)(void);
+//static FX vfx[] = {fl, fb, fgl, fsl, fslb};
+
+
+/*
+Character classes:
+ 0 - continue
+ 1 - special character (in Z0) except '='
+ 2 - Alpha
+ 3 - Numeral
+ 4 - ':'
+ 5 - '<'
+ 6 - '>'
+ 7 - '='
+*/
+
+static char edgeLookupTable[][8] =
+{
+/*      Ct Sp Al Nm :  <  >  = */
+/* 0 */ 0, 0, 1, 2, 3, 4, 5, 0,
+/* 1 */ 0, 0, 1, 1, 0, 0, 0, 0,
+/* 2 */ 0, 0, 0, 2, 0, 0, 0, 0,
+/* 3 */ 0, 0, 0, 0, 0, 0, 0, 6,
+/* 4 */ 0, 0, 0, 0, 0, 0, 0, 7,
+/* 5 */ 0, 0, 0, 0, 0, 0, 0, 8,
+/* 6 */ 0, 0, 0, 0, 0, 0, 0, 0,
+/* 7 */ 0, 0, 0, 0, 0, 0, 0, 0,
+/* 8 */ 0, 0, 0, 0, 0, 0, 0, 0
+};
+
+static FILE*    pIF;
+static tMorph   MorphInit;
+static tMorph   Morph;
+static int      c, state, Z, lastZ, wroteLastC = 1;
+static int      line, col;
+static char     vBuf[1024+1];
+static char*    pBuf;
+static int      checkKeyword(tMorph* m);
